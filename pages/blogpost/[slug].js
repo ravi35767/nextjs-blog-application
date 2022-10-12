0aebdfeb983abc43ebf1d305bae055d82dd2df08
Ravi Shankar <ravi.shankar@inoviotech.local>
2022-10-12 21:28:41 +0500

    Updated blogs

---
+++ pages/blogpost/[slug].js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
+import styles from '../../styles/BlogPost.module.css';
+import * as fs from 'fs';
+// Step 1: Find the file corresponding to the slug
+// Step 2: Populate them inside the page
+
+const Slug = (props) => {
+
+    function createMarkup(c) {
+        return { __html: c };
+    }
+    const [blog, setBlog] = useState(props.myBlog);
+
+    // const router = useRouter();
+    // useEffect(() => {
+    //     if (!router.isReady) return;
+    //     const { slug } = router.query;
+    //     fetch(`http://localhost:3000/api/getBlogs?slug=${slug}`).then((a) => {
+    //         return a.json();
+    //     })
+    //         .then((parsed) => {
+    //             setBlog(parsed)
+    //         })
+    // }, [router.isReady])
+    
+    return <div className={styles.container}>
+        <main className={styles.main}>
+            <h1>{blog && blog.title}</h1>
+            <hr />
+            {blog && <div dangerouslySetInnerHTML={createMarkup(blog.content)}></div>}
+        </main>
+    </div>;
+};
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { slug: 'How-to-learn-javascript'} },
+            { params: { slug: 'how-to-learn-flask'} },
+            { params: { slug: 'how-to-learn-nextjs'} },
+        ],
+        fallback: true // false or 'blocking'
+    };
+}
+export async function getStaticProps(context) {
+    const { slug } = context.params;
+    let myBlog = await fs.promises.readFile(`blogdata/${slug}.json`, 'utf-8')
+    return {
+        props: { myBlog: JSON.parse(myBlog) }, // will be passed to the page component as props
+    }
+}
+export default Slug;
